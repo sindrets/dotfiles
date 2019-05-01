1c3b570bf6edd2a0ea2065954cc395775b271f00
Sindre T. Strøm <sindrets@gmail.com>
2019-05-01 07:37:15 +0200

    added compton conf.

--- .config/i3/increase_volume.sh
+++ .config/i3/increase_volume.sh
@@ -2,5 +2,5 @@
 sink=`pactl list short sinks | grep RUNNING | cut -f1`
 sLength=`expr length "$sink"`
 [ $sLength -eq 0 ] && sink=0
-notify-send "Volume $(pamixer --sink $sink --get-volume-human)" -t 500
 /usr/bin/pactl set-sink-volume $sink '+5%'
+notify-send "Volume $(pamixer --sink $sink --get-volume-human)" -t 500
