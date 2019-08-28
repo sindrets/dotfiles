fd12bea27d0d81a9652885f1f2d1252b84838511
Sindre T. Strøm <sindrets@gmail.com>
2019-08-29 00:04:19 +0200

    Updated polybar launch script

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -5,6 +5,6 @@ killall polybar
 sleep 1;
 
 export monitor_primary="`xrandr | grep primary | cut -d " " -f 1`"
-export ETH="`ip route | grep '^default( .*){3} en' | awk '{print $5}' | head -n1`"
+export ETH="`ip route | grep -P '^default( .*){3} en' | awk '{print $5}' | head -n1`"
 
 polybar -r main >> /home/sindrets/.config/polybar/polybar.log 2>&1 &
