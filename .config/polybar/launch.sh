5c679d1583fe3ff8b342724945c2da680939e145
Sindre T. Strøm <sindrets@gmail.com>
2020-02-22 20:23:50 +0100

    Update

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,4 +7,7 @@ sleep 1;
 export monitor_primary="`xrandr | grep primary | cut -d " " -f 1`"
 export ETH="`ip route | grep -P '^default( .*){3} en' | awk '{print $5}' | head -n1`"
 
+custom_path="$HOME/.config/polybar/custom.ini"
+[ ! -e "$custom_path" ] && touch "$custom_path"
+
 polybar -r main >> /home/sindrets/.config/polybar/polybar.log 2>&1 &
