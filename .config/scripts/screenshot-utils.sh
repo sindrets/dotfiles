1b086104d577eca481d02a4fc8278fb70ff65fdb
Sindre T. Strøm <sindrets@gmail.com>
2020-12-05 00:10:38 +0100

    Update

--- .config/scripts/screenshot-utils.sh
+++ .config/scripts/screenshot-utils.sh
@@ -2,20 +2,20 @@
 target="$HOME/Pictures/screenshots/`date +'%Y-%m-%d-%H%M%S'`_maim.png"
 
 case "$1" in
-	-s)
-		maim -s > "$target"
-		;;
-	-a)
-		maim -i `xdotool getactivewindow` | convert - \
-			\( +clone -background black -shadow 94x12+0+6 \) \
-			+swap -background none -layers merge +repage "$target"
-		;;
+    -s)
+        maim -s "$target"
+        ;;
+    -a)
+        maim -i $(xdotool getactivewindow) | convert - \
+            \( +clone -background black -shadow 94x12+0+6 \) \
+            +swap -background none -layers merge +repage "$target"
+        ;;
 esac
 
-if [ "$?" -eq 0 ]; then
-	# copy to clipboard
-	xclip -selection clipboard -t image/png -i "$target"
+if [ $? -eq 0 ]; then
+    # copy to clipboard
+    xclip -selection clipboard -t image/png -i "$target"
 
-	dunstify "Screenshot" "Screenshot saved to: '$target'" -i "$target"	
+    dunstify "Screenshot" "Screenshot saved to: '$target'" -i "$target" 
 fi
 
