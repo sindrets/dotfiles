d479d7607299dd761adb260d7243f7fc69b5dde6
Sindre T. Strøm <sindrets@gmail.com>
2021-02-11 01:16:06 +0100

    Update

--- .config/polybar/scripts/pulseaudio-tail.sh
+++ .config/polybar/scripts/pulseaudio-tail.sh
@@ -5,11 +5,11 @@ muted=false
 sink=0
 
 # Get the most relevant sink
-update_sink() {	
-	sink=`pactl list short sinks | grep RUNNING | cut -f1`
-	if [ -z "$sink" ]; then
-		sink=`pacmd info | grep "Default sink" | awk '{print $4}'`
-	fi
+update_sink() { 
+    sink=`pactl list short sinks | grep RUNNING | cut -f1`
+    if [ -z "$sink" ]; then
+        sink=`pacmd info | grep "Default sink" | awk '{print $4}'`
+    fi
 }
 
 volume_up() {
@@ -30,19 +30,19 @@ volume_mute() {
 volume_print() {
     update_sink
 
-	active_port=$(pacmd list-sinks | sed -n "/index: $sink/,/index:/p")
-	if [ -z "$active_port" ]; then
-		active_port=`pacmd list-sinks | sed -n "/name: <$sink/,/index:/p"`
-	fi
-	active_port=`echo "$active_port" | grep "active port"`
+    active_port=$(pacmd list-sinks | sed -n "/index: $sink/,/index:/p")
+    if [ -z "$active_port" ]; then
+        active_port=`pacmd list-sinks | sed -n "/name: <$sink/,/index:/p"`
+    fi
+    active_port=`echo "$active_port" | grep "active port"`
 
     if echo "$active_port" | grep -q speaker; then
         # icon=""
-		icon=""
+        icon="󰓃"
     elif echo "$active_port" | grep -Pq "headphones|lineout"; then
-        icon=""
+        icon="󰋋"
     else
-        icon=""
+        icon="󰕾"
     fi
 
     muted=$(pamixer --sink "$sink" --get-mute)
@@ -50,32 +50,32 @@ volume_print() {
     if [ "$muted" = true ]; then
         echo "$icon --"
     else
-		volume=`pamixer --sink "$sink" --get-volume`
+        volume=`pamixer --sink "$sink" --get-volume`
         echo "$icon $volume"
     fi
 }
 
 update_volume () {
-	volume=`pamixer --sink "$sink" --get-volume`
-	muted=`pamixer --sink "$sink" --get-mute`
+    volume=`pamixer --sink "$sink" --get-volume`
+    muted=`pamixer --sink "$sink" --get-mute`
 }
 
 listen() {
     volume_print
 
-	local vLast=$volume
-	local mLast=$muted
-	local sLast=$sink
+    local vLast=$volume
+    local mLast=$muted
+    local sLast=$sink
     pactl subscribe | while read -r event; do
-	if echo "$event" | grep -Pq "('change' on sink)|('new' on source-output)"; then
-			update_volume
-			update_sink
-			if [ "$volume" != "$vLast" ] || [ "$muted" != "$mLast"  ] || [ "$sink" != "$sLast" ]; then 
-				vLast=$volume
-				mLast=$muted
-				sLast=$sink
-				volume_print
-			fi
+    if echo "$event" | grep -Pq "('change' on sink)|('new' on source-output)"; then
+            update_volume
+            update_sink
+            if [ "$volume" != "$vLast" ] || [ "$muted" != "$mLast"  ] || [ "$sink" != "$sLast" ]; then 
+                vLast=$volume
+                mLast=$muted
+                sLast=$sink
+                volume_print
+            fi
         fi
     done
 }
