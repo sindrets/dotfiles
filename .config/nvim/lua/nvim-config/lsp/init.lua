f83f8c0089d9c8be016379c7dc0642e5ec7321b5
Marwan Mustafa Fikrat <marwan.mm@live.com>
2022-06-27 22:18:50 +1200

    feat: add json lsp

--- .config/nvim/lua/nvim-config/lsp/init.lua
+++ .config/nvim/lua/nvim-config/lsp/init.lua
@@ -100,6 +100,9 @@ require('lspconfig').vimls.setup(M.create_config())
 -- Go
 require('lspconfig').gopls.setup(M.create_config())
 
+-- Json
+require'nvim-config.lsp.json'
+
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
     virtual_text = false,
