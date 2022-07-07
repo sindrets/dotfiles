857c7cdae285262071546b7dbc0304ff7ef5b196
Marwan Mustafa Fikrat <marwan.mm@live.com>
2022-07-07 13:28:22 +1200

    fix: nvim merge conflict

--- .config/nvim/.vim/init.lua
+++ .config/nvim/.vim/init.lua
@@ -1,7 +1,2 @@
-<<<<<<< Updated upstream
-vim.env.GIT_WORK_TREE = vim.fn.expand("~")
-vim.env.GIT_DIR = vim.fn.expand("~/.dotfiles")
-=======
 -- vim.env.GIT_WORK_TREE = vim.fn.expand("~")
 -- vim.env.GIT_DIR = vim.fn.expand("~") .. "/.dotfiles"
->>>>>>> Stashed changes
