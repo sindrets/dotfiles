ab75dac203b5af810321862d685700298f73d832
Sindre T. Strøm <sindrets@gmail.com>
2022-08-07 16:26:45 +0200

    (nvim): Custom command aliasing.

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,4 +1,5 @@
 local ok, impatient = pcall(require, "impatient")
+
 if ok then
   impatient.enable_profile()
 end
@@ -28,11 +29,31 @@ _G.pl = Config.common.utils.pl
 Config.lib = require("nvim-config.lib")
 Config.term = require("nvim-config.plugins.term")
 
+local alias = require("nvim-config.plugins.cmd_alias").alias
+local api = vim.api
 local lib = Config.lib
 local utils = Config.common.utils
-local api = vim.api
 
 require("nvim-config")
+
+-- COMMAND ALIASES
+
+alias("brm", "BRemove")
+alias("sch", "Scratch")
+alias("wins", "Windows")
+alias("hh", "HelpHere")
+alias("mh", "ManHere")
+alias("gh", "Git ++curwin")
+alias("T", "Telescope")
+alias("gs", "Telescope git_status")
+alias("gb", "Telescope git_branches")
+alias("gl", "Telescope git_commits")
+alias("Qa", "qa")
+alias("QA", "qa")
+alias("we", "w | e")
+alias("ftd", "filetype detect")
+
+-- FUNCTIONS
 
 Config.fn.toggle_quickfix = lib.create_buf_toggler(
   function()
@@ -77,15 +98,18 @@ local function open_messages_win()
   vim.cmd("belowright sp")
   vim.cmd("wincmd J")
   local bufnr = utils.find_buf_with_var("bufid", "messages_window")
+
   if not bufnr then
     bufnr = api.nvim_create_buf(false, false)
     api.nvim_buf_set_name(bufnr, "Messages")
     api.nvim_buf_set_var(bufnr, "bufid", "messages_window")
   end
+
   local lines = get_messages()
   api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
   api.nvim_set_current_buf(bufnr)
   api.nvim_win_set_height(0, math.min(math.max(#lines, 3), 14))
+
   utils.set_local(0, {
     list = false,
     winfixheight = true,
@@ -95,6 +119,7 @@ local function open_messages_win()
     signcolumn = "no",
     colorcolumn = {},
   })
+
   vim.cmd("norm! G")
 end
 
@@ -103,9 +128,11 @@ function Config.fn.update_messages_win()
     no_hidden = true,
     tabpage = 0,
   })
+
   if bufnr then
     vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, get_messages())
     local winids = utils.win_find_buf(bufnr, 0)
+
     if #winids > 0 then
       api.nvim_set_current_win(winids[1])
       vim.cmd("norm! G")
